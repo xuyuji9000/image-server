46bcc1cfd24da3551993fb3474d12b76506964a3
Karl Xu <karl.yogi.xu@gmail.com>
2017-03-16 18:16:49 +0800

    first commit

---
+++ config/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/home', App\Controllers\FileController::class.':home');
+
+$app->get('/json', function($request, $response, $args){
+    // header("Content-Type: application/json");
+    // header('Access-Control-Allow-Origin: *');
+    $result = [
+        'one'=>1,
+        'three'=>3,
+        'five'=>5
+    ];
+    $response = $response->withJson($result);
+    return $response;
+});
+
+$app->post('/upload', function($request, $response, $args){
+    var_dump($request);
+});
