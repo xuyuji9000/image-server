ad743dc121bdff1cbf0e158cbde07100faffcae9
Karl Xu <karl.yogi.xu@gmail.com>
2017-03-20 12:22:54 +0800

    rm dist/bundle.js.map

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Dropzone from 'react-dropzone';
+
+let DropzoneDemo = React.createClass({
+    onDrop: function(acceptedFiles, rejectedFiles){
+        console.log('Accepted Files:', acceptedFiles);
+        console.log('Rejected files', rejectedFils);
+    },
+    render: function() {
+        return (
+            <div>
+                <Dropzone onDrop={this.onDrop}>
+                    <div>Try drop some files here, or click to select files to upload</div>
+                </Dropzone>
+            </div>
+        );
+    }
+});
+
+const App = (
+    <div>
+        <h1>Hello World!</h1>
+        <DropzoneDemo></DropzoneDemo>
+    </div>
+);
 
 
 render(
-    <h1>Hello World!</h1>,
+    App,
     document.getElementById('root')
 );
