446151fc67099750602e4d733891a913251c76ce
Karl Xu <karl.yogi.xu@gmail.com>
2017-05-02 12:29:50 +0800

    add_backend_setup_command

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ npm run build:prod
 
 run backend api
 ```bash
-php -S localhsot:3001 -t ./public ./public/index.php
+php -S localhost:3001 -t ./public ./public/index.php
 ```
 
 ### upload
--- config/middleware.php
+++ config/middleware.php
@@ -11,6 +11,6 @@ $app->add(new \Slim\Middleware\Session([
 // Cross Domain Access
 $app->add(function($request, $response, $next){
     $response = $next($request, $response);
-    $response = $response->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    $response = $response->withHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
     return $response;
 });
